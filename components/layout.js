d8a8fae112cde71632cd5b830699dc2a78ed4bc4
Brittany Renee Mayes <brmayes16@gmail.com>
2023-06-24 00:02:18 -0400

    add social image and fix conditional

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,6 @@ export const siteTitle = "Brittany Renee Mayes";
 
 export default function Layout({ children, home }) {
   const router = useRouter();
-  console.log(router);
 
   return (
     <div>
@@ -14,16 +13,13 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="All about Brittany Renee Mayes" />
         <meta name="og:title" content={siteTitle} />
-        {/* TODO: UPDATE META DATA */}
-        {/* <meta
+        <meta
           property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content="https://brmayes.github.io/images/social-brm.png"
         />
-        <meta name="twitter:card" content="summary_large_image" /> */}
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
-      <main className={!home && utilStyles.layoutContainer}>
+      <main className={!home ? utilStyles.layoutContainer : undefined}>
         {children}
 
         {!home && (
